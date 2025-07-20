7f660d49495ff180814fcfd6b7e3fe30e0e75373
Rinnegatamante <rinnegatamante@gmail.com>
2025-07-20 16:26:42 +0200

    Added V-Sync injection support.

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ Simple plugin that allows to force a 20/30 FPS framecap on PSVita apps by forcin
 
 ## Changelog
 
+### v.1.1
+
+- Added support for injecting V-Sync into applications not using it.
+
 ### v.1.0
 
 - Initial release.
 
+## Differences between suprx files
+
+- `Framecapper30.suprx`: 30 FPS cap for applications already having V-Sync functionality.
+- `Framecapper20.suprx`: 20 FPS cap for applications already having V-Sync functionality.
+- `Framecapper30Inject.suprx`: 30 FPS cap for applications not already having V-Sync functionality.
+- `Framecapper20Inject.suprx`: 20 FPS cap for applications not already having V-Sync functionality.
+
 ## Setup Instructions
 
-- Install this plugin by copying `Framecapper20.suprx` or `Framecapper30.suprx` (depending on what framerate target you want) to your taiHEN plugins folder (usually `ux0:tai`) and adding entries to your `config.txt` under `*ALL` or under specific Title ID sections you want to use the plugin on.
+- Install this plugin by copying the suprx files (depending on what framerate target you want) to your taiHEN plugins folder (usually `ux0:tai`) and adding entries to your `config.txt` under `*ALL` or under specific Title ID sections you want to use the plugin on.
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <taihen.h>
 
 //#define TARGET_20FPS // Uncomment this to target 20 FPS instead
+//#define INJECT_VSYNC_WAIT // Uncomment this to inject a VSync wait
 
 #ifdef TARGET_20FPS
 #define VSYNC_INTERVAL (3)
@@ -13,6 +14,8 @@ static SceUID sceDisplayWaitVblankStartMulti_hook, sceDisplayWaitVblankStart_hoo
 static SceUID sceDisplayWaitVblankStartMultiCB_hook, sceDisplayWaitVblankStartCB_hook;
 static tai_hook_ref_t sceDisplayWaitVblankStartMulti_ref, sceDisplayWaitVblankStart_ref;
 static tai_hook_ref_t sceDisplayWaitVblankStartMultiCB_ref, sceDisplayWaitVblankStartCB_ref;
+static SceUID sceDisplaySetFrameBuf_hook;
+static tai_hook_ref_t sceDisplaySetFrameBuf_ref;
 
 int sceDisplayWaitVblankStartMulti_patched(unsigned int interval) {
 	return TAI_CONTINUE(int, sceDisplayWaitVblankStartMulti_ref, VSYNC_INTERVAL);
@@ -28,6 +31,12 @@ int sceDisplayWaitVblankStartMultiCB_patched(unsigned int interval) {
 
 int sceDisplayWaitVblankStartCB_patched() {
 	return sceDisplayWaitVblankStartMultiCB(VSYNC_INTERVAL);
+}
+
+int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, SceDisplaySetBufSync sync) {
+	int ret = TAI_CONTINUE(int, sceDisplaySetFrameBuf_ref, pParam, sync);
+	sceDisplayWaitVblankStartMulti(VSYNC_INTERVAL);
+	return ret;
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
@@ -55,7 +64,13 @@ int module_start(SceSize argc, const void *args) {
 						TAI_ANY_LIBRARY,
 						0x78B41B92,
 						sceDisplayWaitVblankStartCB_patched);
-						
+#ifdef INJECT_VSYNC_WAIT						
+	sceDisplaySetFrameBuf_hook = taiHookFunctionImport(&sceDisplaySetFrameBuf_ref,
+						TAI_MAIN_MODULE,
+						TAI_ANY_LIBRARY,
+						0x7A410B64,
+						sceDisplaySetFrameBuf_patched);
+#endif				
 	return SCE_KERNEL_START_SUCCESS;
 }
 
@@ -64,6 +79,9 @@ int module_stop(SceSize argc, const void *args) {
 	taiHookRelease(sceDisplayWaitVblankStart_hook, sceDisplayWaitVblankStart_ref);
 	taiHookRelease(sceDisplayWaitVblankStartMultiCB_hook, sceDisplayWaitVblankStartMultiCB_ref);
 	taiHookRelease(sceDisplayWaitVblankStartCB_hook, sceDisplayWaitVblankStartCB_ref);
+#ifdef INJECT_VSYNC_WAIT
+	taiHookRelease(sceDisplaySetFrameBuf_hook, sceDisplaySetFrameBuf_ref);
+#endif
 
 	return SCE_KERNEL_STOP_SUCCESS;
 	
